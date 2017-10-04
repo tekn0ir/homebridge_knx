19b2265ba57d26056dec926a1665aec06c0ab5d6
Anders Åslund <anders.aslund@teknoir.se>
2017-10-04 21:35:43 +0200

    Change back to regular start script

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-{ dbus-daemon --system --nofork ; avahi-daemon ; su -l -c /home/node/node_modules/.bin/homebridge node ; }
+dbus-daemon --system
+avahi-daemon -D
+su -l -c /home/node/node_modules/.bin/homebridge node
